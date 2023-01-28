06510f3adeeb15800dc91c2636d31f414ef36548
jerry_zhang <jerry.zhang@airbnb.com>
2023-01-28 22:41:58 +0800

    init

---
+++ popup.tsx
@@ -0,0 +1,11 @@
+function IndexPopup() {
+  return (
+    <div>
+      <a href="https://chat.openai.com/chat" target="_blank">
+        ChatGPT Web
+      </a>
+    </div>
+  )
+}
+
+export default IndexPopup
---
+++ request.ts
@@ -0,0 +1,3 @@
+export const PROMOT_KEY = 'PROMOT_KEY'
+
+export const HISTORY_KEY = 'HISTORY_KEY'
